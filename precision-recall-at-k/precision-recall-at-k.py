700c6b4d25e3973b994ee38d4ae2acdbd5641711
Anurodh <85835729+nur0078@users.noreply.github.com>
2026-03-24 18:58:40 +1100

    Update solution for precision-recall-at-k

--- precision-recall-at-k/precision-recall-at-k.py
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -6,19 +6,18 @@ def precision_recall_at_k(recommended, relevant, k):
     # Write code here
     #top-k as a set
     top_k = set(recommended[:k])
-
     #relevant sets so we get O(1) lookup time
     rel_items = set(relevant)
 
     #top_k in relevant set
     intersection = (top_k & rel_items)
 
-    #number of relevant items    
-    num_of_rel = len(relevant)
     #number of hits - common items
     hits = len(intersection)
     
     precision = hits / k
-    recall = hits / num_of_rel
+    recall = hits / len(relevant)
 
     return([precision, recall])
+
+    
